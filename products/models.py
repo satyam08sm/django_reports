a2e5ed26046452f416492dbe7113b4cd0dbd3a7f
satyam08sm <mhatre.satyam619@gmail.com>
2021-04-18 21:04:03 +0530

    Models setup is done

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='products', default='no_picture.png')
+    price = models.FloatField(help_text='in INR')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.created_date.strftime("%d/%m/%Y")}'
--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from profiles.models import Profile
 
-# Create your models here.
+
+class Report(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='reports', blank=True, null=True)
+    remarks = models.TextField(blank=True, null=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
